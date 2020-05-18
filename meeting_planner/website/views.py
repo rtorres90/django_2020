a385f0e5031240a2c30ca1472576eaef7152d047
Roberto Torres <roberto.torres.dev@gmail.com>
2020-05-18 09:09:33 -0400

    more views

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,3 +7,11 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the meeting planner!")
+
+
+def date(request):
+    return HttpResponse(f"Welcome to the meeting planner! the date is {datetime.now()}")
+
+
+def about(request):
+    return HttpResponse("This is the about page!")
